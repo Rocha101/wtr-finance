12bd1133643f87c3febb9cb60bac96a77fdd0c95
rocha101 <eduardo@inovarsistemas.com>
2024-03-27 01:31:10 -0300

    fix: main url

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   env: {
     API_URL: `http${process.env.NEXT_PUBLIC_VERCEL_URL ? "s" : ""}://${
-      process.env.NEXT_PUBLIC_SITE_URL ?? process.env.NEXT_PUBLIC_VERCEL_URL
+      process.env.NEXT_PUBLIC_SITE_URL ?? "wtr-finance.vercel.app"
     }/api`,
   },
   redirects: async () => {

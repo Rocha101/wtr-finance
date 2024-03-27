de7448b531ae86ef8174a4b7d9322495f78a75be
rocha101 <eduardo@inovarsistemas.com>
2024-03-27 01:15:40 -0300

    fix: production

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    API_URL: process.env.API_URL,
+    API_URL: `http${process.env.NEXT_PUBLIC_VERCEL_URL ? "s" : ""}://${
+      process.env.NEXT_PUBLIC_SITE_URL ?? process.env.NEXT_PUBLIC_VERCEL_URL
+    }/api`,
   },
   redirects: async () => {
     return [

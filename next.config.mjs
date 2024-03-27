86265acb1b7f6499bc50827f670242b3e274e305
rocha101 <eduardo@inovarsistemas.com>
2024-03-27 08:42:36 -0300

    fix: api call

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    API_URL: NEXT_PUBLIC_SITE_URL
-      ? `http://${NEXT_PUBLIC_SITE_URL}/api`
+    API_URL: process.env.NEXT_PUBLIC_SITE_URL
+      ? `http://${process.env.NEXT_PUBLIC_SITE_URL}/api`
       : process.env.NEXT_PUBLIC_VERCEL_ENV === "production"
       ? `https://${process.env.MAIN_URL}/api`
       : `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api`,
